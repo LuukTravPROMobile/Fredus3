657eed56fc87461c19b18e93eee32537d5b8f8d7
Luuk Muskens <153828010+Luukmuskens@users.noreply.github.com>
2024-11-13 14:15:02 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,87 +1,46 @@
 import React, { Component } from "react";
-import Card from "./components/card/Card";
 import axios from "axios";
-import Header from "./components/PopUpHeader/PopUpHeader";
-import "./App.css";
-import AddressMap from "./components/AddressMap/AddressMap";
-import Spinner from "./components/Spinner/Spinner";
-
-// const AppDupe = () => {
-//   const [hotels, setHotels] = useState([]);
-//   const [count, setCount] = useState(0);
-
-//   const counter = () => {
-//     setCount(count + 1);
-//   }
-
-//   const fetchData = () => {
-//     // fetch(....)
-//     setHotels('data');
-//   }
-
-//   useEffect(() => {
-//     counter();
-//     fetchData()
-//   },[])
-
-//   useEffect(() => {
-//     fetchData()
-//   },[count])
-
-//   return(
-//     <div>
-//       {!hotels ? 'hotel list is empty' : hotels.map()}
-//       <button>doe iets met click en counter</button>
-//     </div>
-//   )
-// }
-
-//class Header extends Card{
-
-//}
-//<Card.Header></Card.Header>
-// Assuming you have this Card component
+import Spinner from "./Spinner";
+import Header from "./Header";
+import Card from "./Card";
+import AddressMap from "components/AddressMap"; // Import de AddressMap component
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       hotels: [],
-      loading: true, // Start loading true to fetch data immediately
+      loading: true,
       error: null,
     };
   }
 
-  // Call the function to get the data when the component mounts
+  // Haal de data op van de API
   componentDidMount() {
     this.getList();
   }
 
-  // Function to get the data from the API
+  // Functie om de data van de API op te halen
   async getList() {
     try {
       const response = await axios.get(
         "https://travpro.yourworldapps.nl/API/app/v2/listings.php?app=1435&lat1=36.5098445064823&lat2=35.74337885497288&lon1=-114.83208606646728&lon2=-115.48191020892334?category=&query="
-      ); // Replace with your API endpoint
-      console.log("response>", response);
-
+      );
       this.setState({
-        hotels: response.data, // the API returns an array of listings
+        hotels: response.data, // de API geeft een lijst met hotels terug
         loading: false,
       });
     } catch (error) {
       this.setState({
         loading: false,
-        error: error.message, // Set error message if the request fails
+        error: error.message,
       });
     }
   }
 
   render() {
     const { hotels, loading, error } = this.state;
-    console.log("data", hotels);
 
-    // Return loading message if data is still being fetched
     if (loading) {
       return (
         <div>
@@ -90,35 +49,30 @@ class App extends Component {
       );
     }
 
-    // Return error message if data cannot be fetched
     if (error) {
       return <div>Error: {error}</div>;
     }
 
     return (
       <div className="app">
-        {/* Add the Header component here */}
         <Header />
 
-        {/* Map Component */}
+        {/* Voeg de AddressMap component toe en geef de hotels door */}
         <AddressMap hotels={hotels} />
 
-        {/* Cards Section */}
         <div className="card-container">
-          {hotels.map((hotel, index, marker) => {
-            return (
-              <Card
-                key={index}
-                bedrijfsnaam={hotel.company}
-                city={hotel.city}
-                state={hotel.state}
-                addr1={hotel.addr1}
-                web_url={hotel.web_url}
-                phone={hotel.phone}
-                number={String(index + 1)}
-              />
-            );
-          })}
+          {hotels.map((hotel, index) => (
+            <Card
+              key={index}
+              bedrijfsnaam={hotel.company}
+              city={hotel.city}
+              state={hotel.state}
+              addr1={hotel.addr1}
+              web_url={hotel.web_url}
+              phone={hotel.phone}
+              number={String(index + 1)}
+            />
+          ))}
         </div>
       </div>
     );
@@ -126,3 +80,4 @@ class App extends Component {
 }
 
 export default App;
+
