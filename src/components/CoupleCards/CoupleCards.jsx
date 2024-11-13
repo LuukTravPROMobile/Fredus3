c1d8a34690648b09bc1398072f6e35cade7d7cd5
Luuk Muskens <153828010+Luukmuskens@users.noreply.github.com>
2024-11-13 15:28:10 +0100

    change

--- src/components/CoupleCards/CoupleCards.jsx
+++ src/components/CoupleCards/CoupleCards.jsx
@@ -1,45 +1,38 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import { GoogleMap, LoadScript, Marker, InfoWindow } from '@react-google-maps/api';
 
 const MapComponent = () => {
-  const [markers, setMarkers] = useState([]);
   const [selectedMarker, setSelectedMarker] = useState(null);
-
-  const handleMapClick = (event) => {
-    const { lat, lng } = event.latLng.toJSON();
-    const newMarker = {
-      lat,
-      lng,
-      title: `Marker at point (${lat.toFixed(4)}, ${lng.toFixed(4)})`, // Customize title as needed
-    };
-    setMarkers((prevMarkers) => [...prevMarkers, newMarker]);
-  };
-
-  const handleMarkerClick = (marker) => {
-    setSelectedMarker(marker);
-  };
 
   const mapContainerStyle = {
     height: '400px',
     width: '100%',
   };
 
+  const center = selectedMarker
+    ? { lat: selectedMarker.lat, lng: selectedMarker.lng }
+    : { lat: 36.14896463588831, lng: -115.17517089843751 };
+
+  const handleMapClick = useCallback((event) => {
+    const { lat, lng } = event.latLng.toJSON();
+    setSelectedMarker({ lat, lng, title: `Marker at (${lat.toFixed(4)}, ${lng.toFixed(4)})` });
+  }, []);
+
   return (
     <LoadScript googleMapsApiKey="AIzaSyBpfyrBMTrgHH3YnaGHxfjEN_w7OXBJoqc">
       <GoogleMap
         mapContainerStyle={mapContainerStyle}
-        center={markers.length > 0 ? markers[0] : { lat: 36.14896463588831, lng: -115.17517089843751 }} // Center on first marker
+        center={center}
         zoom={13}
         onClick={handleMapClick}
       >
-        {markers.map((marker, index) => (
+        {selectedMarker && (
           <Marker
-            key={index}
-            position={{ lat: marker.lat, lng: marker.lng }}
-            onClick={() => handleMarkerClick(marker)}
+            position={{ lat: selectedMarker.lat, lng: selectedMarker.lng }}
+            onClick={() => setSelectedMarker(null)} // Deselect marker on click
           />
-        ))}
-
+        )}
+        
         {selectedMarker && (
           <InfoWindow
             position={{ lat: selectedMarker.lat, lng: selectedMarker.lng }}
@@ -57,3 +50,4 @@ const MapComponent = () => {
 };
 
 export default MapComponent;
+
