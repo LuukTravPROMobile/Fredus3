33101bf81b527da2d24ac5628f483312524ece73
ChrisTravPRO <chris@travpromobile.com>
2024-11-13 11:38:53 +0100

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
+const el = document.getElementById("root");
 
-
-const el = document.getElementById('root');
-
-//3) tell react to take control of that element
-const root = ReactDOM.createRoot(el);
-//4) create a component
-
-//5) show the component on the screen 
-root.render(
-    <App />
-);
-  
- 
+// Ensure the 'root' element exists before trying to use it
+if (el) {
+  const root = ReactDOM.createRoot(el);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+} else {
+  console.error("Root element not found");
+}
