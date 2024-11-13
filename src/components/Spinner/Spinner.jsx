9ad9920fb1efa75a2085d60f407d85782d95b21e
ChrisTravPRO <chris@travpromobile.com>
2024-11-13 11:55:16 +0100

    Added build scripts + spinner fix

--- src/components/Spinner/Spinner.jsx
+++ src/components/Spinner/Spinner.jsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 const spinnerAnimation = `
 @keyframes spin {
   0% { transform: rotate(0deg); }
@@ -7,26 +9,30 @@ const spinnerAnimation = `
 
 const Spinner = () => {
   return (
-    <>
+    <React.Fragment>
       <style>{spinnerAnimation}</style> {/* Inject the CSS animation */}
-      <div style={{ 
-          display: 'flex', 
-          justifyContent: 'center', 
-          alignItems: 'center', 
-          height: '100vh' 
-        }}>
-        <div style={{ 
-          display: 'inline-block', 
-          width: '80px', 
-          height: '80px', 
-          border: '10px solid rgba(0, 0, 0, 0.1)', 
-          borderTop: '10px solid #3498db', 
-          borderRadius: '50%', 
-          animation: 'spin 5s linear infinite', 
-          color: 'black',
-        }} />
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+        }}
+      >
+        <div
+          style={{
+            display: "inline-block",
+            width: "80px",
+            height: "80px",
+            border: "10px solid rgba(0, 0, 0, 0.1)",
+            borderTop: "10px solid #3498db",
+            borderRadius: "50%",
+            animation: "spin 5s linear infinite",
+            color: "black",
+          }}
+        />
       </div>
-    </>
+    </React.Fragment>
   );
 };
 
