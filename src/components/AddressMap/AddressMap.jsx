1ae3e14a94342a999e0c7dd25ac1610ef62706e8
ChrisTravPRO <chris@travpromobile.com>
2024-11-14 11:53:57 +0100

    Update AddressMap.jsx

--- src/components/AddressMap/AddressMap.jsx
+++ src/components/AddressMap/AddressMap.jsx
@@ -1,4 +1,4 @@
-import { GoogleMap, Marker, LoadScript } from "@react-google-maps/api";
+import { GoogleMap, MarkerF, LoadScript } from "@react-google-maps/api";
 import React from "react";
 
 const mapContainerStyle = {
@@ -35,7 +35,7 @@ const AddressMap = ({ hotels }) => {
           );
           console.log(position);
           return (
-            <Marker
+            <MarkerF
               key={index}
               position={position} // Use position directly
               label={String(index + 1)}
