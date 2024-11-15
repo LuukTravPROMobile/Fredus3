266c3a9077053a9e4a2799b0e0826a5197787662
Luuk Muskens <153828010+Luukmuskens@users.noreply.github.com>
2024-11-15 11:39:30 +0100

    update

--- src/components/MarkersSetup/MarkersSetup.jsx
+++ src/components/MarkersSetup/MarkersSetup.jsx
@@ -3,21 +3,10 @@ import PropTypes from "prop-types";
 import GoogleMapReact from "google-map-react";
 import MapMarker from "./MapMarker.js";
 
-const createMapOptions = () => {
-  return {
-    fullscreenControl: false,
-    mapTypeControl: false,
-    panControl: false,
-    streetViewControl: false,
-    zoomControl: true, // Changed to boolean
-    gestureHandling: "greedy",
-  };
-};
-
-
-  render() {
+cons olidatedMap = () => {
+render() {
     const { locations, hoveredCardId } = this.props;
-
+  
     const mapMarkers = locations.map((location) => (
       <MapMarker
         key={location.id}
@@ -28,6 +17,24 @@ const createMapOptions = () => {
         hoveredCardId={hoveredCardId}
       />
     ));
+  
+    return (
+      <div style={{ width: "100%", height: "100%" }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{
+            key: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+            v: "3.31",
+          }}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+          options={createMapOptions}
+        >
+          {mapMarkers}
+        </GoogleMapReact>
+      </div>
+    );
+  }
+  
 
 
     return (
@@ -46,9 +53,10 @@ const createMapOptions = () => {
           </GoogleMapReact>
         </div>
       );
-    }
+
 MapAndMarkers.propTypes = {
   classes: PropTypes.object.isRequired
 };
+}
 
-export default withStyles(styles)(MapAndMarkers);
+export default olidatedMap(styles)(MapAndMarkers);
