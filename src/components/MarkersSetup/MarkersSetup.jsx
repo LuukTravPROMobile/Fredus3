089a5a570ac61c4802ac0984bd65af7dce29ed13
Luuk Muskens <153828010+Luukmuskens@users.noreply.github.com>
2024-11-15 12:38:50 +0100

    lat veranderd
    ja dat

--- src/components/MarkersSetup/MarkersSetup.jsx
+++ src/components/MarkersSetup/MarkersSetup.jsx
@@ -1,62 +1,61 @@
 import React from "react";
 import PropTypes from "prop-types";
 import GoogleMapReact from "google-map-react";
-import MapMarker from "./MapMarker.js";
+import MapMarker from "./MapMarker";
 
-cons olidatedMap = () => {
-render() {
-    const { locations, hoveredCardId } = this.props;
-  
-    const mapMarkers = locations.map((location) => (
-      <MapMarker
-        key={location.id}
-        lat={location.lat}
-        lng={location.lng}
-        name={location.location_name}
-        pageid={location.pageid}
-        hoveredCardId={hoveredCardId}
-      />
-    ));
-  
-    return (
-      <div style={{ width: "100%", height: "100%" }}>
-        <GoogleMapReact
-          bootstrapURLKeys={{
-            key: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
-            v: "3.31",
-          }}
-          defaultCenter={this.props.center}
-          defaultZoom={this.props.zoom}
-          options={createMapOptions}
-        >
-          {mapMarkers}
-        </GoogleMapReact>
-      </div>
-    );
-  }
-  
+const createMapOptions = () => ({
+  fullscreenControl: false,
+  mapTypeControl: false,
+  panControl: false,
+  streetViewControl: false,
+  zoomControl: true,
+  gestureHandling: "greedy",
+});
 
+const defaultCenter = {
+  lat: 36.14896463588831, // default latitude
+  lng: -115.17517089843751, // default longitude
+}) => (
+  <div style={{ width: "100%", height: "100%" }}>
+    <GoogleMapReact
+      bootstrapURLKeys={{
+        key: "AIzaSyBpfyrBMTrgHH3YnaGHxfjEN_w7OXBJoqc",
+        v: "3.31",
+      }}
+      defaultCenter={center}
+      defaultZoom={zoom}
+      options={createMapOptions}
+    >
+      {locations.map((location) => (
+        <MapMarker
+          key={location.id}
+          lat={location.lat}
+          lng={location.lng}
+          name={location.location_name}
+          pageid={location.pageid}
+          hoveredCardId={hoveredCardId}
+        />
+      ))}
+    </GoogleMapReact>
+  </div>
+);
 
-    return (
-        <div style={{ width: "100%", height: "100%" }}>
-          <GoogleMapReact
-            bootstrapURLKeys={{
-              key:"AIzaSyBpfyrBMTrgHH3YnaGHxfjEN_w7OXBJoqc",// Secure the API key
-              v: "3.31",
-            }}
-            defaultCenter={this.props.center}
-            defaultZoom={this.props.zoom}
-            hoverDistance={10} // Adjusted for clarity
-            options={createMapOptions}
-          >
-            {mapMarkers}
-          </GoogleMapReact>
-        </div>
-      );
-
-MapAndMarkers.propTypes = {
-  classes: PropTypes.object.isRequired
+ConsolidatedMap.propTypes = {
+  locations: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      lat: PropTypes.number.isRequired,
+      lng: PropTypes.number.isRequired,
+      location_name: PropTypes.string,
+      pageid: PropTypes.string,
+    })
+  ).isRequired,
+  hoveredCardId: PropTypes.string,
+  center: PropTypes.shape({
+    lat: PropTypes.number,
+    lng: PropTypes.number,
+  }),
+  zoom: PropTypes.number,
 };
-}
 
-export default olidatedMap(styles)(MapAndMarkers);
+export default ConsolidatedMap;
