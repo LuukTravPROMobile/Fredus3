ada328842ab1cd447fd6a5911b4bf750dbad0408
Luuk Muskens <153828010+Luukmuskens@users.noreply.github.com>
2024-11-15 15:00:02 +0100

    update

--- src/components/MarkersSetup/MarkersSetup.jsx
+++ src/components/MarkersSetup/MarkersSetup.jsx
@@ -2,17 +2,22 @@ import React from "react";
 import PropTypes from "prop-types";
 import GoogleMapReact from "google-map-react";
 import MapMarker from "./MapMarker";
-import Card from './card';
+import Card from "./card";
 const defaultCenter = {
   lat: 36.14896463588831, // default latitude
   lng: -115.17517089843751, // default longitude
 };
 
-const MapComponent = ({ locations, hoveredCardId, center = defaultCenter, zoom = 10 }) => (
+const MapComponent = ({
+  locations,
+  hoveredCardId,
+  center = defaultCenter,
+  zoom = 10,
+}) => (
   <div style={{ width: "100%", height: "100%" }}>
     <GoogleMapReact
       bootstrapURLKeys={{
-        key: "AIzaSyBpfyrBMTrgHH3YnaGHxfjEN_w7OXBJoqc", 
+        key: "AIzaSyBpfyrBMTrgHH3YnaGHxfjEN_w7OXBJoqc",
         v: "3.31",
       }}
       defaultCenter={center}
